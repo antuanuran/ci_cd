cfc30d49b258b7390d1b76d141c26c7a8e6a74b0
Antuan_Uranov <uranovanton@gmail.com>
2023-12-05 12:20:18 +0300

    2

---
+++ apps/basket/views.py
@@ -0,0 +1,30 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from apps.basket.models import Basket, BasketRow
+from apps.basket.serializers import BasketRowSerializer
+from apps.orders.premissions import IsOwner
+
+
+@method_decorator(
+    name="create",
+    decorator=swagger_auto_schema(
+        operation_description='Необходимо ввести только 2 параметра: {"qty": 1, "tovar_id": 1}',
+    ),
+)
+class BasketRowViewSet(ModelViewSet):
+    queryset = BasketRow.objects.all()
+    serializer_class = BasketRowSerializer
+
+    def perform_create(self, serializer):
+        basket, _ = Basket.objects.get_or_create(user_id=self.request.user.id)
+        # print("2. perform_create - Перед сохранением")
+        serializer.save(basket_id=basket.id)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        result = qs.filter(basket__user_id=self.request.user.id)
+        return result
---
+++ apps/orders/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from rest_framework.exceptions import PermissionDenied
+
+from apps.orders.models import Order, OrderRow
+from apps.products.serializers import ItemParameterSerializer, ItemSerializer, ProductSerializer
+
+
+class OrderDetailSerializer(ItemSerializer):
+    parameters = ItemParameterSerializer(read_only=True, many=True)
+    product = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+
+class OrderRowSerializer(serializers.ModelSerializer):
+    item = OrderDetailSerializer(read_only=True)
+
+    class Meta:
+        model = OrderRow
+        fields = ["qty", "price", "item"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    spisok_tovarov_zakaza = OrderRowSerializer(read_only=True, many=True, source="rows")
+    order_id = serializers.IntegerField(source="id")
+
+    class Meta:
+        model = Order
+        fields = [
+            "order_id",
+            "status",
+            "created_at",
+            "updated_at",
+            "spisok_tovarov_zakaza",
+        ]
+
+    def validate_status(self, value):
+        if value != Order.STATUS_CANCELED:
+            raise PermissionDenied("User может изменять статус заказа только на 'отменён'")
+        return value
